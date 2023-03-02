8a7fd51ac538b0ecb85ac5c10532ec0ea03dbd04
MartinJ-13 <80908021+MartinJ-13@users.noreply.github.com>
2023-03-02 16:25:09 +0800

    Initial commit

---
+++ app/src/main/java/com/mobdeve/s13/group8/arellano_ngo_romero/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s13.group8.arellano_ngo_romero.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
