3dab23545c5f127951acd61d168dbdb8daeb8de9
Nathan Ngo <nathan_ngo@dlsu.edu.ph>
2023-03-13 15:57:09 +0800

    Added Dashboard and Filter Restaurants

---
+++ app/src/main/java/com/mobdeve/s13/group8/arellano_ngo_romero/myapplication/RestaurantModel.kt
@@ -0,0 +1,12 @@
+package com.mobdeve.s13.group8.arellano_ngo_romero.myapplication
+
+data class RestaurantModel(val name: String,
+                           val location: String,
+                           val diningType: String,
+                           val cuisineType: String,
+                           val imageResId: Int){
+
+}
+
+
+
