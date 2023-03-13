f7ecc17b40eb16227a7ecdcd67ef4dd1823a6462
nathan_ngo <nathan_ngo@dlsu.edu.ph>
2023-03-13 16:30:08 +0800

    Renamed Activity
    Renamed recyclerview related activities for anything related to the RestaurantPreview view

--- app/src/main/java/com/mobdeve/s13/group8/arellano_ngo_romero/myapplication/RestaurantPreviewHome.kt
+++ app/src/main/java/com/mobdeve/s13/group8/arellano_ngo_romero/myapplication/RestaurantPreviewHome.kt
@@ -1,20 +1,17 @@
 package com.mobdeve.s13.group8.arellano_ngo_romero.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.mobdeve.s13.group8.arellano_ngo_romero.myapplication.databinding.ActivityHomepageDashboardBinding
 
 
-class HomeDashboard : AppCompatActivity() {
+class RestaurantPreviewHome : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val viewBinding: ActivityHomepageDashboardBinding =
             ActivityHomepageDashboardBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-
 
     }
 }
--- app/src/main/java/com/mobdeve/s13/group8/arellano_ngo_romero/myapplication/RestaurantPreviewViewHolder.kt
+++ app/src/main/java/com/mobdeve/s13/group8/arellano_ngo_romero/myapplication/RestaurantPreviewViewHolder.kt
@@ -1,4 +1,4 @@
 package com.mobdeve.s13.group8.arellano_ngo_romero.myapplication
 
-class ViewHolder {
+class RestaurantPreviewViewHolder {
 }
