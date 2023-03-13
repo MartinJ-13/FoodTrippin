4d950cb7c436ee79ed8d7a3b3e5a92a0b0bd4ef7
MartinJ-13 <80908021+MartinJ-13@users.noreply.github.com>
2023-03-13 18:43:47 +0800

    fixed bug for recyclerview reviews

--- app/src/main/java/com/mobdeve/s13/group8/arellano_ngo_romero/myapplication/ReviewViewHolder.kt
+++ app/src/main/java/com/mobdeve/s13/group8/arellano_ngo_romero/myapplication/ReviewViewHolder.kt
@@ -6,11 +6,11 @@ import com.mobdeve.s13.group8.arellano_ngo_romero.myapplication.databinding.Prof
 class ReviewViewHolder(private val viewBinding: ProfilemyreviewsLayoutBinding ): RecyclerView.ViewHolder(viewBinding.root) {
 
     fun bindData(review: Review){
-        this.viewBinding.myReviewsReviewTv.text = review.review
-        this.viewBinding.myReviewsTitleTv.text = review.title
-        this.viewBinding.myReviewsDatePostedTv.text = review.date
-        this.viewBinding.likedUserIconIv.setImageResource(review.imageId)
-        this.viewBinding.myReviewsReviewImage1Iv.setImageResource(review.reviewPicID1)
-        this.viewBinding.myReviewsReviewImage2Iv.setImageResource(review.reviewPicID2)
+        this.viewBinding.reviewReviewTv.text = review.review
+        this.viewBinding.reviewTitleTv.text = review.title
+        this.viewBinding.reviewDatePostedTv.text = review.date
+        this.viewBinding.reviewUserIconIv.setImageResource(review.imageId)
+        this.viewBinding.reviewReviewImage1Iv.setImageResource(review.reviewPicID1)
+        this.viewBinding.reviewReviewImage2Iv.setImageResource(review.reviewPicID2)
     }
 }
