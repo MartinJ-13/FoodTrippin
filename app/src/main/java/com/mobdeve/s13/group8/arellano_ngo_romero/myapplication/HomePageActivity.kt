d0b849e3dec474f820b21d14bd7e36ec7536115b
nathan_ngo <nathan_ngo@dlsu.edu.ph>
2023-03-16 23:53:21 +0800

    Filter popup fixed, Homepage Activity intent to RestaurantActivity

--- app/src/main/java/com/mobdeve/s13/group8/arellano_ngo_romero/myapplication/HomePageActivity.kt
+++ app/src/main/java/com/mobdeve/s13/group8/arellano_ngo_romero/myapplication/HomePageActivity.kt
@@ -9,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mobdeve.s13.group8.arellano_ngo_romero.myapplication.databinding.ActivityHomepageBinding
-import com.mobdeve.s13.group8.arellano_ngo_romero.myapplication.databinding.ActivityToolbarBinding
 import com.mobdeve.s13.group8.arellano_ngo_romero.myapplication.databinding.PopupRestaurantfilterBinding
 
 class HomePageActivity : AppCompatActivity() {
@@ -18,7 +17,7 @@ class HomePageActivity : AppCompatActivity() {
     private lateinit var myAdapter: RestaurantPreviewAdapter
     private lateinit var viewBinding: ActivityHomepageBinding
     private lateinit var recyclerView: RecyclerView
-    private lateinit var toolbarBinding: ActivityToolbarBinding
+
     private lateinit var popupBinding: PopupRestaurantfilterBinding
     private lateinit var popupWindow: PopupWindow
 
@@ -33,48 +32,54 @@ class HomePageActivity : AppCompatActivity() {
         recyclerView.adapter = myAdapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        toolbarBinding = ActivityToolbarBinding.inflate(layoutInflater)
-        setSupportActionBar(toolbarBinding.toolbar)
-
-        toolbarBinding.filterBtn.setOnClickListener {
-            popupBinding = PopupRestaurantfilterBinding.inflate(layoutInflater)
-
-            val cuisineOptions = arrayOf("American", "Japanese", "Chinese", "Mexican", "Korean")
-            val diningOptions = arrayOf("Fine Dining", "Casual Dining", "Fast Food", "Buffet", "Food Court")
-            val options = arrayOf(cuisineOptions, diningOptions)
-
-            val spinnerAdapters = mutableListOf<SpinnerAdapter>()
-
-            for (option in options) {
-                spinnerAdapters.add(SpinnerAdapter(this, option))
-            }
-
-            popupBinding.btnApply.setOnClickListener{
-                // TODO: Implement filtering logic
-                popupWindow.dismiss()
-            }
-
-            for (i in spinnerAdapters.indices) {
-                val adapter = spinnerAdapters[i]
-                val spinner = when (i) {
-                    0 -> popupBinding.spCuisineType
-                    1 -> popupBinding.spDiningType
-                    else -> null
-                }
-                spinner?.adapter = adapter
-            }
-
-            popupWindow = PopupWindow(
-                popupBinding.root,
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                true
-            )
-
-            val toolbarLayout = toolbarBinding.root.findViewById<LinearLayout>(R.id.toolbarLayout)
-            toolbarLayout.addView(toolbarBinding.filterBtn)
-
-            popupWindow.showAtLocation(popupBinding.root, Gravity.CENTER, 0, 0)
+        // Show filter popup on filter button click
+        viewBinding.filterBtn.setOnClickListener {
+            showFilterPopup()
         }
+    }
+
+    private fun showFilterPopup() {
+        popupBinding = PopupRestaurantfilterBinding.inflate(layoutInflater)
+
+        val cuisineOptions = arrayOf("--Choose a Cuisine--","American", "Japanese", "Chinese", "Mexican", "Korean")
+        val diningOptions = arrayOf("--Choose a Dining Option--", "Fine Dining", "Casual Dining", "Fast Food", "Buffet", "Food Court")
+        val options = arrayOf(cuisineOptions, diningOptions)
+
+        val spinnerAdapters = mutableListOf<SpinnerAdapter>()
+
+        for (option in options) {
+            spinnerAdapters.add(SpinnerAdapter(this, option))
+        }
+
+        popupBinding.btnApply.setOnClickListener{
+            // TODO: Implement filtering logic
+            popupWindow.dismiss()
+        }
+
+        for (i in spinnerAdapters.indices) {
+            val adapter = spinnerAdapters[i]
+            val spinner = when (i) {
+                0 -> popupBinding.spCuisineType
+                1 -> popupBinding.spDiningType
+                else -> null
+            }
+            spinner?.adapter = adapter
+        }
+
+        popupWindow = PopupWindow(
+            popupBinding.root,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            true
+        )
+
+        // Dismiss popup on close button click
+        popupBinding.closePopupBtn.setOnClickListener {
+            popupWindow.dismiss()
+        }
+
+        // Show popup window
+        val rootView = window.decorView.rootView
+        popupWindow.showAtLocation(rootView, Gravity.CENTER, 0, 0)
     }
 }
--- app/src/main/java/com/mobdeve/s13/group8/arellano_ngo_romero/myapplication/RestaurantPreviewAdapter.kt
+++ app/src/main/java/com/mobdeve/s13/group8/arellano_ngo_romero/myapplication/RestaurantPreviewAdapter.kt
@@ -1,25 +1,32 @@
 package com.mobdeve.s13.group8.arellano_ngo_romero.myapplication
 
+import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.content.ContextCompat.startActivity
 import com.mobdeve.s13.group8.arellano_ngo_romero.myapplication.databinding.ItemlayoutRestaurantcardBinding
 
 class RestaurantPreviewAdapter (private val data: ArrayList<RestaurantPreviewModel>) : RecyclerView.Adapter<RestaurantPreviewViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestaurantPreviewViewHolder {
-        val itemViewBinding: ItemlayoutRestaurantcardBinding = ItemlayoutRestaurantcardBinding.inflate(
-            LayoutInflater.from(parent.context), parent, false
-        )
+
+        val itemViewBinding: ItemlayoutRestaurantcardBinding = ItemlayoutRestaurantcardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
         return RestaurantPreviewViewHolder(itemViewBinding)
     }
 
     override fun onBindViewHolder(holder: RestaurantPreviewViewHolder, position: Int) {
         holder.bindData(data[position])
+
+        holder.itemView.setOnClickListener{
+            val intent = Intent(holder.itemView.context, RestaurantActivity::class.java)
+            holder.itemView.context.startActivity(intent)
+        }
     }
     override fun getItemCount(): Int {
         return data.size
     }
 }
-
 
